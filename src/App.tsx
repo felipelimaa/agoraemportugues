71f87a2fffdd5c1914a54cc40cb84ccbac826f05
Felipe Lima <felipe.lima@unimednatal.com.br>
2022-03-26 15:42:23 -0300

    Created Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,39 @@
+import Navbar from "./components/Navbar"
+
 function App() {
   return (
-    <p>Agora em Português</p>
+    <>
+      <Navbar />
+      <div id="Titulo">
+        <h1>Marina Beatryz Dantas Costa</h1>
+        <p>Revisão de Texto</p>
+      </div>
+
+      <div id="Formacao">
+        <h3>Formação</h3>
+        <p><strong>Instituição</strong></p>
+        <p>Conteudo 1</p>
+        <p>Conteudo 2</p>
+        <h3>Formação</h3>
+        <p><strong>Instituição</strong></p>
+        <p>Conteudo 1</p>
+        <p>Conteudo 2</p>
+        <h3>Formação</h3>
+        <p><strong>Instituição</strong></p>
+        <p>Conteudo 1</p>
+        <p>Conteudo 2</p>
+      </div>
+
+      <div id="Portfolio">
+        <h3>Trabalhos acadêmicos</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
+          ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+          It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop 
+          publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      </div>
+
+    </>
   )
 }
 
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,27 @@
+
+import {SiLinkedin, SiInstagram} from 'react-icons/si';
+
+import "./styles.css";
+
+const Navbar = () => {
+	return (
+		<div id="Navbar">
+			<div></div>
+			<div id="Navbar-button">
+				<button>INÍCIO</button>
+				<button>TEXTOS</button>
+				<button>CONTATO</button>
+			</div>
+			<div id="Navbar-icon">
+				<a href="https://www.linkedin.com/in/professoramarinacosta/" target={'_blank'}>
+					<SiLinkedin style={{backgroundColor: '#fff'}} />
+				</a>
+				<a href="https://www.instagram.com/agoraemportugues/" target={'_blank'}>
+					<SiInstagram />
+				</a>
+			</div>
+		</div>
+	)
+}
+
+export default Navbar
