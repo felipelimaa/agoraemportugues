4844d6b2cecefd0cc354d4509abf6c1b14eae020
Felipe Lima <felipe.lima@unimednatal.com.br>
2022-03-26 16:19:36 -0300

    fix: added @media screen for hide social networks icons to smartphones

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,13 @@
+
+import './styles.css'
+
+const Title = () => {
+    return (
+        <div id="Title">
+            <h1>MARINA BEATRYZ DANTAS COSTA</h1>
+            <h3>REVISÃO DE TEXTO</h3>
+        </div>
+    )
+}
+
+export default Title;
